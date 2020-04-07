f28fd8dbdbfc205a17cd88c6f3385761a94eeac5
Chris Campbell <cacampbell@ucdavis.edu>
2020-04-07 10:35:01 -0700

    Modify command line runner to just create the entities without printing / subscribing

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,7 +2,6 @@ package app;
 
 import app.db.HcsEntityDao;
 import app.domain.Entity;
-import app.service.HederaConsensusService;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -26,34 +25,19 @@ public class Application {
         application.run(args);
     }
 
-    @Bean
-    CommandLineRunner demo(
-        HcsEntityDao dao, 
-        HederaConsensusService hcs
-    ) {
+    @Bean // Bean Constructors are Autowired
+    CommandLineRunner demo(HcsEntityDao dao) {
         return args -> {
-            hcs.subscribe(System.out);
-            System.out.println("\n\n\n");
-            System.out.println("Deleting All Nodes");
             dao.deleteAll();
-            System.out.println("All Nodes: " + dao.findAll());
-            Entity category1 = new Entity();
-            Entity category2 = new Entity();
-            Entity category3 = new Entity();
-            Entity category4 = new Entity();
-            Entity category5 = new Entity();
-            Entity[] entities = new Entity[]{ category1, category2, category3, category4, category5 };
-            System.out.println("Saving");
+            Entity entity1 = new Entity();
+            Entity entity2 = new Entity();
+            Entity entity3 = new Entity();
+            Entity entity4 = new Entity();
+            Entity entity5 = new Entity();
+            entity2.setChildren(new HashSet<>(Arrays.asList(entity1, entity4, entity5)));
+            entity3.setChildren(new HashSet<>(Arrays.asList(entity5, entity2)));
+            Entity[] entities = new Entity[]{ entity1, entity2, entity3, entity4, entity5 };
             for (Entity e : entities) { dao.save(e); }
-            System.out.println("All Nodes: " + dao.findAll());
-            System.out.println("Assigning Children");
-            category2.setChildren(new HashSet<>(Arrays.asList(category1, category4, category5)));
-            category3.setChildren(new HashSet<>(Arrays.asList(category5, category2)));
-            System.out.println("Saving");
-            for (Entity e : entities) { dao.save(e); }
-            System.out.println("All Nodes: " + dao.findAll());
-            System.out.println("\n\n\n");
-            // System.exit(0); // Shut down after adding entities
         };
     }
 }
